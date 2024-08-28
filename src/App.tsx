db0bfb55d55e3b9f0b40f6d2bda08166b145b7a7
Cristian Nass <cristian.tirdad@gmail.com>
2024-08-28 23:01:43 +0200

    implemented Zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import SignupPage from './pages/SignupPage'
-import LogInPage from './pages/LogInPage'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import SignupPage from "./pages/SignupPage";
+import LogInPage from "./pages/LogInPage";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LogInPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route path='/login' element={<LogInPage />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
